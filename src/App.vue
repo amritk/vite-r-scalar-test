cf18f09b195b61793ec2b0efe77e36c434d7cb38
Amrit <amrit@hockey-community.com>
2024-03-15 10:55:34 -0700

    chore: testing vite

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ApiReference } from "@scalar/api-reference";
+</script>
+<template>
+  <ApiReference
+    :configuration="{
+      spec: {
+        url: 'https://petstore.swagger.io/v2/swagger.json',
+      },
+    }"
+  />
+</template>
